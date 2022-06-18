370c533a49c6d80b98f7aeb80dc1fcb7b2fa3c9e
CDrummond <craig.p.drummond@gmail.com>
2022-06-18 07:55:54 +0100

    Revery to using docker, and add changes from chincheta0815 - #3

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -5,16 +5,18 @@ set -eux
 uname -a
 DESTDIR=/src/releases
 
-for d in armhf-linux aarch64-linux ; do
+for d in armhf-linux aarch64-linux; do
     mkdir -p $DESTDIR/$d
-    rm -f $DESTDIR/$d/*
+    rm -rf $DESTDIR/$d/*
 done
 
 function build {
 	echo Building for $1 to $3...
 
 	if [[ ! -f /build/$1/release/bliss-analyser ]]; then
-		cargo build --release --target $1
+		export RUST_BACKTRACE=full
+		export PKG_CONFIG=${1//unknown-/}-pkg-config
+		BINDGEN_EXTRA_CLANG_ARGS="--sysroot /usr/${1//unknown-/}" cargo build --release --target $1
 	fi
 
 	$2 /build/$1/release/bliss-analyser && cp /build/$1/release/bliss-analyser $DESTDIR/$3
@@ -22,5 +24,4 @@ function build {
 
 build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip armhf-linux/bliss-analyser
 build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip aarch64-linux/bliss-analyser
-
 
