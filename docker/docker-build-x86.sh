777d8e3c6cf2de54a7124cc8259018829213670e
chincheta0815 <chincheta0815@users.noreply.github.com>
2024-02-19 18:16:09 +0100

    make ubuntu a docker build for ffmpeg5

---
+++ docker/docker-build-x86.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## #!/usr/bin/env bash
+set -eux
+
+uname -a
+DESTDIR=/src/releases
+
+mkdir -p $DESTDIR/bin
+rm -rf $DESTDIR/bin/*
+
+export RUST_BACKTRACE=full
+cargo build --release
+
+strip /build/release/bliss-analyser && cp /build/release/bliss-analyser $DESTDIR/bliss-analyser-x86-ffmpeg5
+
+cp UserGuide.md $DESTDIR/README.md
+cp LICENSE $DESTDIR/
+cp configs/linux.ini $DESTDIR/config.ini
+cp scripts/bliss-analyser-arm $DESTDIR/bliss-analyser
