e23aba88e31df31e2152c0432d67980811436281
CDrummond <craig.p.drummond@gmail.com>
2025-03-02 08:57:08 +0000

    Have ffmpeg output to a pipe, and read from that - saves creating temp files.

--- src/ffmpeg.rs
+++ src/ffmpeg.rs
@@ -9,26 +9,40 @@
 use bliss_audio::decoder::Decoder as DecoderTrait;
 use bliss_audio::decoder::PreAnalyzedSong;
 use bliss_audio::BlissResult;
-use std::fs;
+use byteorder::{LittleEndian, ReadBytesExt};
 use std::path::Path;
-use std::process::Command;
-use md5;
-
+use std::process::{Command, Stdio};
+use std::io;
 pub struct FFmpegCmdDecoder;
 
 impl DecoderTrait for FFmpegCmdDecoder {
     fn decode(path: &Path) -> BlissResult<PreAnalyzedSong> {
         let mut decoded_song = PreAnalyzedSong::default();
-        let digest = md5::compute(path.to_str().unwrap_or("null").as_bytes().to_vec());
-        let tmp_path = format!("/tmp/{:x}.wav", digest);
-        let _ = Command::new("ffmpeg").arg("-i").arg(path).arg("-ar").arg("22050").arg("-ac").arg("1").arg("-c:a").arg("pcm_f32le").arg(tmp_path.clone()).output();
-        let cloned_path = tmp_path.clone();
-        let wav_file = Path::new(&cloned_path);
-        if wav_file.exists() {
-            let mut reader = hound::WavReader::open(tmp_path).unwrap();
-            decoded_song.sample_array = reader.samples::<f32>().flatten().collect();
-            let _ = fs::remove_file(wav_file);
+        if let Ok(mut child) = Command::new("ffmpeg")
+                               .arg("-hide_banner")
+                               .arg("-loglevel").arg("panic")
+                               .arg("-i").arg(path)
+                               .arg("-ar").arg("22050")
+                               .arg("-ac").arg("1")
+                               .arg("-c:a")
+                               .arg("pcm_f32le")
+                               .arg("-f").arg("f32le")
+                               .arg("pipe:1")
+                               .stdout(Stdio::piped())
+                               .spawn() {
+            let stdout = child.stdout.as_mut().expect("Failed to capture stdout");
+            let mut reader = io::BufReader::new(stdout);
+            let mut buffer: Vec<f32> = Vec::new();
+
+            while let Ok(sample) = reader.read_f32::<LittleEndian>() {
+                buffer.push(sample);
+            }
+
+            if let Ok(_) = child.wait() {
+                decoded_song.sample_array = buffer;
+            }
         }
+
         Ok(decoded_song)
     }
 }
