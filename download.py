d42f28d1f29c0e6764ab8e9bd87cb343cc03869e
CDrummond <craig.p.drummond@gmail.com>
2025-09-23 17:07:46 +0100

    Update for reduced set of builds.

--- download.py
+++ download.py
@@ -11,10 +11,10 @@ import datetime, os, requests, shutil, subprocess, sys, tempfile, time
 
 GITHUB_TOKEN_FILE = "%s/.config/github-token" % os.path.expanduser('~')
 GITHUB_REPO = "CDrummond/bliss-analyser"
-LINUX_ARM_ARTIFACTS = ["bliss-analyser-linux-arm-ffmpeg", "bliss-analyser-linux-arm-static", "bliss-analyser-debian-bullseye-arm-libav", "bliss-analyser-debian-bookworm-arm-libav", "bliss-analyser-linux-arm-symphonia"]
-LINUX_X86_ARTIFACTS = ["bliss-analyser-linux-x86-ffmpeg", "bliss-analyser-linux-x86-static", "bliss-analyser-ubuntu-22.04-x86-libav", "bliss-analyser-ubuntu-24.04-x86-libav", "bliss-analyser-linux-x86-symphonia"]
-MAC_ARTIFACTS =       ["bliss-analyser-mac-ffmpeg", "bliss-analyser-mac-symphonia"]
-WINDOWS_ARTIFACTS =   ["bliss-analyser-windows-libav", "bliss-analyser-windows-symphonia"]
+LINUX_ARM_ARTIFACTS = ["bliss-analyser-linux-arm-ffmpeg", "bliss-analyser-linux-arm-symphonia"]
+LINUX_X86_ARTIFACTS = ["bliss-analyser-linux-x86-ffmpeg", "bliss-analyser-linux-x86-symphonia"]
+MAC_ARTIFACTS =       ["bliss-analyser-mac-symphonia"]
+WINDOWS_ARTIFACTS =   ["bliss-analyser-windows-ffmpeg", "bliss-analyser-windows-symphonia"]
 UNIX_ARTIFACTS = LINUX_ARM_ARTIFACTS + LINUX_X86_ARTIFACTS + MAC_ARTIFACTS
 GITHUB_ARTIFACTS = UNIX_ARTIFACTS + WINDOWS_ARTIFACTS
 
