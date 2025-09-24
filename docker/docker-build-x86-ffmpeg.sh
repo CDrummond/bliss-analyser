d7da5dcd95a5332cb12352d9790317271f298436
CDrummond <craig.p.drummond@gmail.com>
2025-09-24 22:18:46 +0100

    Use docker for Linxu builds and link to musl

--- docker/docker-build-x86-ffmpeg.sh
+++ docker/docker-build-x86-ffmpeg.sh
@@ -5,23 +5,18 @@ set -eux
 uname -a
 DESTDIR=/src/releases
 
-mkdir -p $DESTDIR/bin
-rm -rf $DESTDIR/bin/*
-
 function build {
 	echo Building for $1 to $3...
 
     if [[ ! -f /build/$1/release/bliss-analyser ]]; then
-        cargo build --release --features=symphonia --target $1
+        cargo build --release --features=libav,staticlibav --target $1
     fi
 
     $2 /build/$1/release/bliss-analyser && cp /build/$1/release/bliss-analyser $DESTDIR/$3
 }
 
-build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip bin/bliss-analyser-armhf
-build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip bin/bliss-analyser-aarch64
+build x86_64-unknown-linux-musl strip bliss-analyser
 
 cp UserGuide.md $DESTDIR/README.md
 cp LICENSE $DESTDIR/
 cp configs/linux.ini $DESTDIR/config.ini
-cp scripts/bliss-analyser-arm $DESTDIR/bliss-analyser
--- docker/docker-build-x86-symphonia.sh
+++ docker/docker-build-x86-symphonia.sh
@@ -5,9 +5,6 @@ set -eux
 uname -a
 DESTDIR=/src/releases
 
-mkdir -p $DESTDIR/bin
-rm -rf $DESTDIR/bin/*
-
 function build {
 	echo Building for $1 to $3...
 
@@ -18,10 +15,8 @@ function build {
     $2 /build/$1/release/bliss-analyser && cp /build/$1/release/bliss-analyser $DESTDIR/$3
 }
 
-build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip bin/bliss-analyser-armhf
-build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip bin/bliss-analyser-aarch64
+build x86_64-unknown-linux-musl strip bliss-analyser
 
 cp UserGuide.md $DESTDIR/README.md
 cp LICENSE $DESTDIR/
 cp configs/linux.ini $DESTDIR/config.ini
-cp scripts/bliss-analyser-arm $DESTDIR/bliss-analyser
